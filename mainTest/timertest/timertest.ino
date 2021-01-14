9e367facb79dff37d95eeac7d1975c2b3333560e
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-14 17:07:33 +0100

    something
    wser

--- mainTest/timertest/timertest.ino
+++ mainTest/timertest/timertest.ino
@@ -1,7 +1,5 @@
 
-
 #include <Timer5.h>
-
 
 volatile int counter = 0;
 
@@ -11,25 +9,37 @@ float fs = 500.0;
 float t;
 
 void setup() {
+<<<<<<< Updated upstream
   Serial.begin(115200);
   pinMode(10, OUTPUT);
  
   MyTimer5.begin(10000);
+=======
+  Serial.begin(9600);
+
+  MyTimer5.begin(1000);
+>>>>>>> Stashed changes
   MyTimer5.attachInterrupt(counterIncrease);
+  MyTimer5.start();
 
   //first wave yeye
-  for(int i = 0; i<501; i++){
-    t = (float)i/fs;
-    sig[i] = (int)1023 * (1/2 * (sin(2*PI*f*t)+1));
-    }
-  
-}
-
-void counterIncrease(){
-    counter++;
+  for (int i = 0; i < 1000; i++) {
+    t = (float)i / fs;
+    sig[i] = (0.5 * (sin(2 * PI * f * t) + 1));
   }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+
+}
+
+void counterIncrease() {
+  if (counter < 1000){
+    counter++;
+    }else{
+      counter = 0;
+      }
+}
+
+void loop() {
+  Serial.println(counter);
 }
--- snakke/sjak/sjak.ino
+++ snakke/sjak/sjak.ino
@@ -1,6 +1,5 @@
 
 #include <LiquidCrystal.h>
-#include <string.h>
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //pins connected to LCD
 
@@ -18,22 +17,35 @@ void setup()
 //String msg = "";
 
 void loop() {
-
   String msg = Serial.readString();
+
   if (msg != "") {
     lcd.clear();
-    
     if (msg.length() >= 16) {
-      //print first 16 chars on first line, then shift to next
-      lcd.print(msg.substring(0, 16).c_str());
-      lcd.setCursor(0, 1);
-      lcd.print(msg.substring(16).c_str());
+      String slice = msg.substring(0, 16);
+      lcd.print(slice.c_str());
+      slice = msg.substring(16);
+      lcd.setCursor(0, 1);  
+      lcd.print(slice.c_str());
     } else {
-      lcd.print(msg);
+      lcd.print(msg.c_str());
     }
-
-
-
-
   }
 }
+
+//void loop() {
+//
+//  String msg = Serial.readString();
+//  if (msg != "") {
+//    lcd.clear();
+//
+//    if (msg.length() >= 16) {
+//      //print first 16 chars on first line, then shift to next
+//      lcd.print(msg.substring(0, 16).c_str());
+//      lcd.setCursor(0, 1);
+//      lcd.print(msg.substring(16).c_str());
+//    } else {
+//      lcd.print(msg);
+//    }
+//  }
+//}
