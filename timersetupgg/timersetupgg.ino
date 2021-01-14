4c78aa1eee0bee5fb9f9c307f368d9774ac457f0
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-14 14:27:19 +0100

    test lateer
    for testing later

--- timersetupgg/timersetupgg.ino
+++ timersetupgg/timersetupgg.ino
@@ -1,12 +1,14 @@
 
+
 #include <Timer5.h>
 
 
 volatile int counter = 0;
 
-float mess1 = 0;
+int mess1 = 0;
 
 int annMess1[100];
+int voltage[100];
 
 void setup() {
   Serial.begin(115200);
@@ -21,25 +23,25 @@ void setup() {
 
 void counterIncrease() {
   analogReadResolution(10); //readings will now be done in 10 bits
-  mess1 = analogRead(A1); //
-  for (int i = 0; i = 100, i++) {
-    if(mess1 != annMess1[i-1]){
-      annMess1[i] = mess1;
-      }
+  for (int i = 0; i < 100; i++) {
+    mess1 = analogRead(A1);
+    if (i == 99) {
+      annMess1[99] = mess1;
+    } else {
+      annMess1[i] = annMess1[i + 1];
+    }
+    annMess1[i] = voltage[i];
   }
-
-  //
 
 
   counter++;
 }
 
 void loop() {
-  analogReadResolution(10);
 
-
-
-
+for(int i =0; i < 100; i++){
+  Serial.println(annMess1[i]);
+  }
 
 
 }
