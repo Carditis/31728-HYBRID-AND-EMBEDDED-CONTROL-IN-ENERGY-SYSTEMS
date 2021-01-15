8e994d39ec90ef4c1018f841f77d4cad2af0dd15
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-15 02:29:36 +0100

    Stuff still missing
    Add voltage meas to the lowpass filter

--- timersetupgg/timersetupgg.ino
+++ timersetupgg/timersetupgg.ino
@@ -1,47 +1,86 @@
 
-
 #include <Timer5.h>
 
-//commit5
 volatile int counter = 0;
+int processedCounter = 0;
 
-int mess1 = 0;
+volatile int mess1 = 0;
 
-int annMess1[100];
-int voltage[100];
+float currentY = 0.0;
+float oldY = 0.0;
+float cutoff = 50.0;
+float RC = 1.0 / (2.0 * PI*cutoff);
+float dt = 1.0 / 10000.0;
+float alpha = dt / dt * RC;
+
+float crossOffPoint = 176.0;
+int zeroCrossCounter = 0;
+
 
 void setup() {
   Serial.begin(115200);
   pinMode(10, OUTPUT);
 
-  // MyTimer5.begin(10000);
-  // MyTimer5.attachInterrupt(counterIncrease);
+  analogReadResolution(10); //readings will now be done in 10 bits
 
-
-
+  MyTimer5.begin(10000);
+  MyTimer5.attachInterrupt(takingMeasurements);
 }
 
-void counterIncrease() {
-  analogReadResolution(10); //readings will now be done in 10 bits
-  for (int i = 0; i < 100; i++) {
+void takingMeasurements() {
+
+  mess1 = analogRead(A1);
+  oldY = currentY;
+  currentY = alpha * mess1 + (1 - alpha) * oldY;  //lowpassing here
+  if (oldY < crossOffPoint and currentY >= crossOffPoint) {
+    zeroCrossCounter++;
+  }
+
+  //zero cross here
+
+  /*
     mess1 = analogRead(A1);
+    for (int i = 0; i < 100; i++) {
     if (i == 99) {
       annMess1[99] = mess1;
     } else {
       annMess1[i] = annMess1[i + 1];
     }
-    annMess1[i] = voltage[i];
-  }
-
+    }
+  */
 
   counter++;
 }
 
+
+
 void loop() {
+  analogWrite(A0, mess1);
+  if (processedCounter < counter) {
+    processedCounter = counter;
+    Serial.println((mess1 / 1023.0) * 3.3);
 
-for(int i =0; i < 100; i++){
-  Serial.println(annMess1[i]);
   }
+  /*
+    while (processedCounter < counter) {
+    // mess1 = analogRead(A1);
+    for (int i = 0; i < 100; i++) {
+      if (i == 99) {
+        annMess1[99] = mess1;
+      } else {
+        annMess1[i] = annMess1[i + 1];
+      }
+    }
+    fvoltage = (mess1 / 1023.0) * 3.3;
+    Serial.println(fvoltage);
+    processedCounter++;
+    }
+  */
+  /*
+    noInterrupts();
 
+    memcpy(voltage, annMess1, sizeof(annMess1[0]) * 100); //we copy a array to another array, so it does not get effected by the interrupter.
+    interrupts();
+  */
 
 }
