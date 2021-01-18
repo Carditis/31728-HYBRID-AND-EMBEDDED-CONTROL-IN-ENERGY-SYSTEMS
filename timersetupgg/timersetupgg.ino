edd80a62729ae6d5b1303e08d032cafd438e2550
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-18 16:11:17 +0100

    We got freq
    Freq calculation is now a working :+1:

--- timersetupgg/timersetupgg.ino
+++ timersetupgg/timersetupgg.ino
@@ -13,6 +13,7 @@ int processedCounter = 0;
 
 volatile int mess1 = 0;
 
+//lowpass and zerocross variables
 float currentY = 0.0;
 float oldY = 0.0;
 float cutoff = 50.0;
@@ -20,29 +21,43 @@ float RC = 1.0 / (2.0 * PI*cutoff);
 float dt = 1.0 / 10000.0;
 float alpha = dt / dt * RC;
 
-float crossOffPoint = 176.0; //
+float crossOffPoint = 1023.0/2; //
 int zeroCrossCounter = 0;
+
+//frequnce variables
+unsigned long oldTime = 0;
+unsigned long newTime = 0;
+int zeroCrossInterval = 100;
+float scaleFactor = 1.00015;
+double averageIntervalTime = 0.0;
 
 
 void setup() {
   Serial.begin(115200);
 
+  AdcBooster();
+
   analogReadResolution(10); //readings will now be done in 10 bits
   analogWriteResolution(10);
 
-  MyTimer5.begin(1000);
+  MyTimer5.begin(10000);
   MyTimer5.attachInterrupt(takingMeasurements);
+
+  oldTime = millis();
 }
 
 void takingMeasurements() {
 
   mess1 = analogRead(A2);
-  analogWrite(A0, mess1);
+  //analogWrite(A0, mess1);
   oldY = currentY;
   currentY = alpha * mess1 + (1 - alpha) * oldY;  //lowpassing here
-  if (oldY < crossOffPoint and currentY >= crossOffPoint) {
+  analogWrite(A0, currentY);
+
+  if (oldY > crossOffPoint and currentY <= crossOffPoint) {
     zeroCrossCounter++;
   }
+
 
   //zero cross here
 
@@ -60,17 +75,33 @@ void takingMeasurements() {
   counter++;
 }
 
+void AdcBooster()
+{
+  ADC->CTRLA.bit.ENABLE = 0;                     // Disable ADC
+  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
+  ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 |   // Divide Clock by 64.
+                   ADC_CTRLB_RESSEL_12BIT;       // Result on 12 bits
+  ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 |   // 1 sample
+                     ADC_AVGCTRL_ADJRES(0x00ul); // Adjusting result by 0
+  ADC->SAMPCTRL.reg = 0x00;                      // Sampling Time Length = 0
+  ADC->CTRLA.bit.ENABLE = 1;                     // Enable ADC
+  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
+} // AdcBooster
+
 
 
 void loop() {
   //analogWrite(A0, mess1);
+  //Serial.println("loop");
   if (processedCounter < counter) {
     processedCounter = counter;
     voltage = (mess1 / 1023.0) * 3.3;
-    Serial.println(mess1);
+    //Serial.println(mess1);
     //Serial.println(voltage);
   }
-  delay(400);
+  if (zeroCrossCounter == zeroCrossInterval) {
+    Serial.println(freqCalculator());
+  }
 
   //digitalPins for writing on the LCD, and making LED turn on and off
   //need a check for if the frequency is correct.
@@ -96,4 +127,12 @@ void loop() {
     memcpy(voltage, annMess1, sizeof(annMess1[0]) * 100); //we copy a array to another array, so it does not get effected by the interrupter.
     interrupts();
   */
+}
+
+double freqCalculator() {
+  newTime = millis();
+  averageIntervalTime = ((double)(newTime - oldTime) / (double)(zeroCrossInterval));
+  oldTime = newTime;
+  zeroCrossCounter = 0;
+  return ((1 / averageIntervalTime) * 1000) * scaleFactor ;
 }
