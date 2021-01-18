3744ec8a8f40918011fae1c6403ba67f151aa4c1
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-18 12:25:02 +0100

    Det virker lidt nu
    nu virker det lidt

--- timersetupgg/timersetupgg.ino
+++ timersetupgg/timersetupgg.ino
@@ -2,6 +2,12 @@
 
 #include <Timer5.h>
 
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //pins connected to LCD
+
+float voltage = 0;
+
 volatile int counter = 0;
 int processedCounter = 0;
 
@@ -19,19 +25,19 @@ int zeroCrossCounter = 0;
 
 
 void setup() {
-  Serial.begin(9600);
-  Serial.print("det virker");
-  
+  Serial.begin(115200);
+
   analogReadResolution(10); //readings will now be done in 10 bits
   analogWriteResolution(10);
 
-  MyTimer5.begin(10000);
+  MyTimer5.begin(1000);
   MyTimer5.attachInterrupt(takingMeasurements);
 }
 
 void takingMeasurements() {
 
-  mess1 = analogRead(A1);
+  mess1 = analogRead(A2);
+  analogWrite(A0, mess1);
   oldY = currentY;
   currentY = alpha * mess1 + (1 - alpha) * oldY;  //lowpassing here
   if (oldY < crossOffPoint and currentY >= crossOffPoint) {
@@ -57,14 +63,18 @@ void takingMeasurements() {
 
 
 void loop() {
-  Serial.println("loop");
-  analogWrite(A0, mess1);
+  //analogWrite(A0, mess1);
   if (processedCounter < counter) {
-    Serial.println(counter);
     processedCounter = counter;
-    Serial.println((mess1 / 1023.0) * 3.3);
-
+    voltage = (mess1 / 1023.0) * 3.3;
+    Serial.println(mess1);
+    //Serial.println(voltage);
   }
+  delay(400);
+
+  //digitalPins for writing on the LCD, and making LED turn on and off
+  //need a check for if the frequency is correct.
+
   /*
     while (processedCounter < counter) {
     // mess1 = analogRead(A1);
@@ -85,5 +95,5 @@ void loop() {
 
     memcpy(voltage, annMess1, sizeof(annMess1[0]) * 100); //we copy a array to another array, so it does not get effected by the interrupter.
     interrupts();
-*/
+  */
 }
