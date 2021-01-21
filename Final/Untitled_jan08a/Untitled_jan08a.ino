8d650ee8b8756f1ad145b6b64dab4832037c7ec6
Carditis <45385019+Carditis@users.noreply.github.com>
2021-01-21 21:20:45 +0100

    Comment and cleaning
    title

--- Final/Untitled_jan08a/Untitled_jan08a.ino
+++ Final/Untitled_jan08a/Untitled_jan08a.ino
@@ -6,14 +6,16 @@
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //pins connected to LCD
 
+//variables for calculating the RMS voltage
 float squaredVoltage = 0.0;
 float voltageSummed = 0.0;
 float RMSVoltage = 0.0;
 int RMSCounter = 0;
 
+//main couter and mesuament variable
 volatile int counter = 0;
-
 volatile int mess1 = 0;
+
 //PWM Charging Signal Variables
 float k = 0.002702702702702741;       //Droop constant (Given in slides)
 float f0 = 49.883783783783784;        //Frequency intercept (Given in slides)
@@ -21,6 +23,7 @@ float a1 = 0.6133333333333333;        //PWM slop 1 (46/75)
 float b1 = -0.13333333333332575;      //PWM intercept 1 (6-(10*a1))
 float a2 = 2.5;                       //PWM slope 2 (27.5/11)
 float b2 = -160.0;                    //PWM intercept 2 (52,5-(85*a2))
+float PWMSignal;
 
 
 //LEDs
@@ -35,24 +38,19 @@ float RC = 1.0 / (2.0 * PI*cutoff);
 float dt = 1.0 / 10000.0;
 float alpha = dt / (dt + RC);
 float minusAlpha = 1 - alpha;
-
 float crossOffPoint = 1023.0 / 2; //
 volatile int zeroCrossCounter = 0;
 
 //frequnce variables
 int zeroCrossInterval = 100;
-float scalingFactor = 1.092;
+float scalingFactor = 1.092; //used for stabelizing the freq to its know value
 float averageIntervalTime = 0.0;
-
 float freq = 0.0;
-volatile int PWMCounter = 0;
-float PWMSignal;
-
-//int interruptCheckPin = 1;
 
 
 void setup() {
   Serial.begin(115200);
+  //setting up the LEDs
   pinMode(chargingLED, OUTPUT);
   pinMode(dechargingLED, OUTPUT);
 
@@ -72,59 +70,41 @@ void setup() {
   analogReadResolution(10); //readings will now be done in 10 bits
   analogWriteResolution(10); //same
 
-  MyTimer5.begin(10000);
+  MyTimer5.begin(10000); //the timer will interrupt the system 10000 per seacond. This is made possible with the Adcbooster
   MyTimer5.attachInterrupt(takingMeasurements);
-
-
-  //  pinMode(interruptCheckPin, OUTPUT);
 }
 
 void takingMeasurements() {
-  //for cheking if any interrups are skipped
+  //for cheking if any interrups are skipped, the freq meassured from this, kan be used to determen if the interrupt runs correctly
   //digitalWrite(interruptCheckPin, HIGH);
   //digitalWrite(interruptCheckPin, LOW);
 
-  mess1 = analogRead(A2);
-  //analogWrite(A0, mess1);
+  mess1 = analogRead(A2); //taking the anlog meassument
 
   currentY = alpha * mess1 + (minusAlpha) * oldY;  //lowpassing here
   //analogWrite(A0, currentY);
 
+  //zero cross gets cheked for here
   if (oldY > crossOffPoint and currentY <= crossOffPoint) {
     zeroCrossCounter++;
   }
   oldY = currentY;
   counter++;
-  PWMCounter++;
 }
-
-void AdcBooster()
-{
-  ADC->CTRLA.bit.ENABLE = 0;                     // Disable ADC
-  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
-  ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 |   // Divide Clock by 64.
-                   ADC_CTRLB_RESSEL_12BIT;       // Result on 12 bits
-  ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 |   // 1 sample
-                     ADC_AVGCTRL_ADJRES(0x00ul); // Adjusting result by 0
-  ADC->SAMPCTRL.reg = 0x00;                      // Sampling Time Length = 0
-  ADC->CTRLA.bit.ENABLE = 1;                     // Enable ADC
-  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
-} // AdcBooster
-
-
-
 void loop() {
-  //Serial.println(mess1);
+  //updating the cloud too often, result in a slow running program. This if statement makes sure that does not happen
   if (counter > 20000) {
     ArduinoCloud.update();
   }
 
+  //only calculating the freq after a surtain amount of zero crosses, to metigate errors
   if (zeroCrossCounter >= zeroCrossInterval) {
     freq = freqCalculator();
     lcd.setCursor(7, 0);
     lcd.print(freq, 3);
     frequency = freq;
   }
+  //visually showing if the frequency is in the correct interval, using LEDs on the breadbord
   if (freq < 49.9) {
     digitalWrite(chargingLED, LOW);
     digitalWrite(dechargingLED, HIGH);
@@ -152,18 +132,12 @@ void loop() {
     rMSVALUE = RMSVoltage;
   }
 
-  PWMChargingAmount();
+  PWMChargingAmount(); //this function calculates the PWM signal
 
-  if (PWMCounter < (PWMSignal * 100) * 10000) {
-    analogWrite(A0, 1023);
-  } else {
-    analogWrite(A0, 0);
-  }
-  if (PWMCounter >= 10000) {
-    PWMCounter = 0;
-  }
+  //here we write it to the ociliscope, to confirm its square wave pattern
+  analogWrite(A3, (PWMSignal / 100) * 1023);
 }
-
+//calculating the frequancy based on the amount of zero crosses and time gone by
 float freqCalculator() {
   float tempFreq = (zeroCrossInterval / (counter * 0.0001)) * scalingFactor;
   zeroCrossCounter = 0;
@@ -186,7 +160,6 @@ void PWMChargingAmount() {
     chargingAmp = 0.0;
   }
   //calculating the corresponding PWM signal, from the slopes in IEC61851
-
   if (chargingAmp == 0.0) {
     PWMSignal = 0.0;
   } else if (chargingAmp < 52.0 and chargingAmp > 6) {
@@ -196,7 +169,20 @@ void PWMChargingAmount() {
   } else if (chargingAmp >= 52.0 and chargingAmp < 52.5) {
     PWMSignal = 85 ;
   }
-  currentChargingAmp = "Charge current: " + String(chargingAmp) + "   PWM Signal: " + String(PWMSignal);
+  currentChargingAmp = "Charge current: " + String(chargingAmp) + "   PWM Signal: " + String(PWMSignal); //this will get displayed on the dashboard
+}
+
+void AdcBooster()
+{
+  ADC->CTRLA.bit.ENABLE = 0;                     // Disable ADC
+  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
+  ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 |   // Divide Clock by 64.
+                   ADC_CTRLB_RESSEL_12BIT;       // Result on 12 bits
+  ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 |   // 1 sample
+                     ADC_AVGCTRL_ADJRES(0x00ul); // Adjusting result by 0
+  ADC->SAMPCTRL.reg = 0x00;                      // Sampling Time Length = 0
+  ADC->CTRLA.bit.ENABLE = 1;                     // Enable ADC
+  while ( ADC->STATUS.bit.SYNCBUSY == 1 );       // Wait for synchronization
 }
 void onManualOverWriteChange() {
   // Do something
